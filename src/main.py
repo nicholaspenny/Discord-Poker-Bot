e8c0684cd654eb69dede37956dc99aa688035e10
nicholaspenny <156978686+nicholaspenny@users.noreply.github.com>
2025-08-03 22:47:22 -0400

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import bot
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+os.makedirs("./logs", exist_ok=True)
+
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[
+            RotatingFileHandler("./logs/bot.log", maxBytes=1_000_000, backupCount=3),
+            logging.StreamHandler()
+        ]
+    )
+
+
+def main():
+    # run the bot
+    setup_logging()
+
+    logger = logging.getLogger(__name__)
+    logger.info("Launching FiendBot")
+
+    bot.run_discord_bot()
+
+
+if __name__ == '__main__':
+    main()
