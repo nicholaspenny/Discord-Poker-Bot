100f2c6528a3133a8b9281dc0d274b69f1f4cefc
nicholaspenny <156978686+nicholaspenny@users.noreply.github.com>
2025-09-08 20:36:25 -0400

    Update common.py

--- src/common.py
+++ src/common.py
@@ -2,7 +2,8 @@ CHANNELS_TEMPLATE = {
     'admin', 'commands', 'database', 'email', 'email-database', 'game', 'game-test', 'graph',
     'graph-test', 'ledgers', 'ledgers-test', 'manage', 'music', 'query', 'query-test', 'roles'
 }
-ROLES_TEMPLATE = {'fiend', 'admin', 'fiend bot', 'email needed'}
+ROLES_TEMPLATE = {'star', 'admin', 'poker bot', 'email needed'}
 
 channels: dict[int: dict[str, int]] = {}
+
 roles: dict[int: dict[str, int]] = {}
